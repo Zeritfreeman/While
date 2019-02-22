b911c7f67e22881a661248c6d19a890ecebc0356
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-02-22 01:39:25 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12Caras
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random aleatorio = new Random();
+
+            int total = 0, dado = 0, valorEntero = (int)(dado / 2);
+            double valorDecimal = (double)(dado / 2);
+            string continuar = "s";
+
+            bool condicionalA = total < 100 && continuar == "s";
+            bool condicionalB = valorDecimal != valorEntero;
+
+            while (condicionalA)
+            {
+                for (int i = 0; i <= 53; i++)
+                {
+                    dado = aleatorio.Next(1, 13);
+                    Console.WriteLine("Dado: " + dado);
+                    total += dado;
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("¿Desea lanzar de nuevo (s/n)?: ");
+                    continuar = Console.ReadLine();
+                    if (i < 4) continue;
+                    if (condicionalB) break;
+                }
+            }
+            Console.WriteLine("Termino el juego");
+        }
+    }
+}
